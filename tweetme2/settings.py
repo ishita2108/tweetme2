8317dd224404d159098c84690d36934bb07c6e89
ishita2108 <iwinner21@gmail.com>
2021-05-04 15:27:10 +0530

    REST API VIEWS

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = 'ta3w8%tyodr#=&q3y#-)9(ys%j@$pby%$q!s*c^vyv&272mplg'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
+LOGIN_URL = '/login'
+MAX_TWEET_LENGTH = 240
 
 
 # Application definition
@@ -38,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third-party
+    'rest_framework',
+    #internal
     'tweets',
 ]
 
@@ -120,3 +125,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_RENDERER_CLASSES =[
+    'rest_framework.renderers.JSONRenderer',
+    
+]
+if DEBUG:
+    DEFAULT_RENDERER_CLASSES += ['rest_framework.renderers.BrowsableAPIRenderer',]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES  
+}
--- tweets/views.py
+++ tweets/views.py
@@ -4,18 +4,77 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render,redirect
 from django.utils.http import is_safe_url
 
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
+from . serializers import TweetSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
+    # print(request.user or None)
     # print(args, kwargs)
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context = {}, status = 200)
 
+@api_view(['POST']) #http method the client has to send = POST
+# @authentication_classes([SessionAuthentication]) its already be given in drf 
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        # print(obj)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
+
+
+@api_view(['GET'])
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def tweet_detail_view(request,tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    obj = qs.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data, status=200)
+
+
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated])
+def tweet_delete_view(request,tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    qs = qs.filter(user=request.user)
+    if not qs.exists():
+        return Response({'message':'You cannot delete this tweet!!'}, status=401)
+    obj = qs.first()
+    obj.delete()
+    # serializer = TweetSerializer(obj)
+    return Response({'message':'Tweet Removed!!'}, status=200)
+
+
+
+def tweet_create_view_pure_django(request, *args, **kwargs):
+    '''
+    REST API Create View -> DRF
+    '''
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
+        if request.is_ajax():
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
     # print("ajax",request.is_ajax())
     form = TweetForm(request.POST or None)
     # print('post data is',request.POST)
@@ -29,6 +88,7 @@ def tweet_create_view(request, *args, **kwargs):
         # or if you want to use one of the specialized model saving options. commit is True by default.
 
         #do other form related things
+        obj.user = user
         obj.save()
         if request.is_ajax():
             return JsonResponse(obj.serialize(), status=201)  #201 == created items
@@ -40,7 +100,7 @@ def tweet_create_view(request, *args, **kwargs):
             return JsonResponse(form.errors, status=400)
     return render(request, 'components/forms.html', context={'form': form})
 
-def tweet_list_view(request, *args, **kwargs):
+def tweet_list_view_pure_django(request, *args, **kwargs):
     """
     REST API VIEW
     consumed by JavaScrpt/ Swift/ Java/ iOS/ Android
@@ -55,7 +115,7 @@ def tweet_list_view(request, *args, **kwargs):
     return JsonResponse(data)
 
 
-def tweet_detail_view(request,tweet_id, *args, **kwargs):
+def tweet_detail_view_pure_django(request,tweet_id, *args, **kwargs):
     """
     REST API VIEW
     consumed by JavaScrpt/ Swift/ Java/ iOS/ Android

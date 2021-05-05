564d13afd29c1dc1378f9d0480dcfd0dd56f0722
ishita2108 <iwinner21@gmail.com>
2021-05-05 14:29:08 +0530

    LIKE UNLIKE RETWEET

--- tweets/views.py
+++ tweets/views.py
@@ -10,7 +10,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .forms import TweetForm
 from .models import Tweet
-from . serializers import TweetSerializer
+from . serializers import TweetSerializer, TweetActionSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
@@ -63,6 +63,34 @@ def tweet_delete_view(request,tweet_id, *args, **kwargs):
     # serializer = TweetSerializer(obj)
     return Response({'message':'Tweet Removed!!'}, status=200)
 
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def tweet_action_view(request,*args, **kwargs):
+    ''' 
+    id is required
+    Action options are like, unlike and retweet
+    '''
+    # print(request.data)
+    serializer = TweetActionSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        data = serializer.validated_data
+        tweet_id = data.get("id")
+        action = data.get("action")
+        qs = Tweet.objects.filter(id=tweet_id)
+        if not qs.exists():
+            return Response({}, status=404)
+        obj = qs.first()
+        if action == 'like':
+            obj.likes.add(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
+        elif action == 'unlike':
+            obj.likes.remove(request.user)
+        elif action == 'retweet':
+            pass
+            
+        # serializer = TweetSerializer(obj)
+    return Response({}, status=200)
 
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
